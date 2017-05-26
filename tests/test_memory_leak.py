a8d5c8639701d9c2f645fb251a5e118d890b333b
Silvana Trindade <syletri@gmail.com>
2017-05-26 09:44:23 -0300

    Adding regex to test memory leak

--- tests/test_memory_leak.py
+++ tests/test_memory_leak.py
@@ -14,17 +14,14 @@ import re
 filename = ""
 result = None
 expected = []
-unexpected = []
 
 @scenario('memory_leak.feature', 'Code with memory leak')
 def test_code_with_memory_leak():
     pass
 
-
 @scenario('memory_leak.feature', 'Code without memory leak')
 def test_code_without_memory_leak():
     pass
-
 
 @given('the code doesn\'t have memory leak')
 def doesnt_have_memory_leak():
@@ -32,29 +29,30 @@ def doesnt_have_memory_leak():
     global expected
 
     filename = './support/case2_memory_leak.txt'
-    expected = ["a2.c"]
+    expected = ""
 
-
-@given('the code has memory leaks')
+@given('the code has memory leak')
 def has_memory_leak():
     global filename
-    global expected
 
     filename = './support/case1_memory_leak.txt'
-    expected = ["a1.c"]
-
+    
 @when('it is submitted to the app')
 def submitted():
-
    subprocess.check_output('../main.py ' + filename, shell=True)
-
 
 @then('I should receive the following message "[<filename>.c:<linha>]: (erro) vazamento de memória"')
 def receive_message():
-   pass
+    global filename
 
+    with open("output.txt",'r') as f_out:
+
+        for line in f_out:
+            m = re.search('[\[\]\:\w\.\_]*\s(\(erro\) vazamento de memória)', line)
+            assert m != None
 
 @then('I shouldn\'t receive any messages')
-def receive_any_messages():
-    pass
-
+def receive_no_messages():
+    with open("output.txt",'r') as f_out:
+        contents = f_out.read().strip()
+        assert len(contents) == 0
