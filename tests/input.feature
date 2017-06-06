6bfaa76b07843588ecba95d73ce0a3e8851e40d1
yusselimendez <yusselimendez@users.noreply.github.com>
2017-06-06 16:03:38 -0300

    Fixing input test

--- tests/input.feature
+++ tests/input.feature
@@ -1,18 +1,17 @@
 Feature: Input
     An app that reads a file that has a list of code files
 
-Scenario: All files have errors reported by cppcheck 
-    Given case1_input.txt contains a list of files: a1.c and a2.c
-    When I call the app with the file as parameter
-    Then shows me a list of files with errors: a1.c
-
+Scenario: All files have errors reported by cppcheck
+    Given case1_input.txt contains a file list with programming errors: a1.c and a2.c
+    When I call the app with the container file as parameter
+    Then shows me a file list with programming errors: a1.c, a2.c
 
 Scenario: Not all files have errors reported by cppcheck
-	Given case2_input.txt contains a list of good and bad files: a1.c and a2.c
-	When I call the app with the file as parameter
-	Then shows me a list of files with errors: a1.c
+  	Given case2_input.txt contains a file with programming errors and a file without programming errors: a1.c and a2.c
+  	When I call the app with the container file as parameter
+  	Then shows me a file list with programming errors: a1.c
 
-Scenario: No files have errors reported by cppcheck
-	Given case3_input.txt contains a list of good file: a1.c
-	When I call the app with the file as parameter
-	Then shows me "[a1.c]: Nenhum erro de análise estática foi encontrado"
+Scenario: Files without errors reported by cppcheck
+  	Given case3_input.txt contains a file list without programming errors: a1.c
+  	When I call the app with the container file as parameter
+  	Then shows me "[a1.c]: Nenhum erro de análise estática foi encontrado"
--- tests/test_input.py
+++ tests/test_input.py
@@ -8,7 +8,7 @@ from pytest_bdd import (
     when,
 )
 
-import subprocess 
+import subprocess
 import re
 
 filename = ""
@@ -17,27 +17,53 @@ expected = []
 unexpected = []
 
 @scenario('input.feature', 'All files have errors reported by cppcheck')
-def test_input_file_with_errors():
+def test_all_files_have_errors():
     pass
 
-@given('case1_input.txt contains a list of files: a1.c and a2.c')
-def file_with_list_of_files():
+
+@given('case1_input.txt contains a file list with programming errors: a1.c and a2.c')
+def file_list_with_programming_errors_a1c_and_a2c():
     global filename
     global expected
 
     filename = './support/case1_input.txt'
     expected = ["a1.c", "a2.c"]
 
-@when('I call the app with the file as parameter')
+
+@when('I call the app with the container file as parameter')
 def call_the_app():
-   global result
+    global result
 
-   result = subprocess.check_output('../susy-avalia.py ' + filename + ' > output.txt', shell=True)
+    result = subprocess.check_output('../susy-avalia.py ' + filename + ' > output.txt', shell=True)
+
+@then('shows me a file list with programming errors: a1.c, a2.c')
+def shows_me_a_file_list_with_programming_errors_a1c_a2c():
+    global expected
+
+    with open("output.txt",'r') as f_out:
+        contents = f_out.read()
+
+        for f in expected:
+            assert f in contents
 
 
-@then('shows me a list of files with errors: a1.c')
-def shows_errors():
+@scenario('input.feature', 'Not all files have errors reported by cppcheck')
+def test_not_all_files_have_errors():
+    pass
 
+@given('case2_input.txt contains a file with programming errors and a file without programming errors: a1.c and a2.c')
+def file_with_programming_errors_and_a_file_without_programming_errors_a1c_and_a2c():
+    global filename
+    global expected
+    global unexpected
+
+    filename = './support/case2_input.txt'
+    expected = ["a1.c"]
+    unexpected = ["a2.c"]
+
+
+@then('shows me a file list with programming errors: a1.c')
+def shows_me_a_file_list_with_programming_errors_a1c():
     global filename
     global expected
     global unexpected
@@ -51,41 +77,28 @@ def shows_errors():
         for f in unexpected:
             assert ("[" + f + "]: Nenhum erro de análise estática foi encontrado") in contents
 
-@scenario('input.feature', 'Not all files have errors reported by cppcheck')
-def test_input_file_without_errors():
+
+@scenario('input.feature', 'Files without errors reported by cppcheck')
+def test_files_without_errors():
     pass
 
 
-@given('case2_input.txt contains a list of good and bad files: a1.c and a2.c')
-def file_with_list_of_good_and_bad_files():
-    global filename
-    global expected
-    global unexpected
-
-    filename = './support/case2_input.txt'
-    expected = ["a1.c"]
-    unexpected = ["a2.c"]
-
-@scenario('input.feature', 'No files have errors reported by cppcheck')
-def test_no_errors():
-    pass
-
-@given('case3_input.txt contains a list of good file: a1.c')
-def file_with_list_of_good_files():
+@given('case3_input.txt contains a file list without programming errors: a1.c')
+def file_list_without_programming_errors_a1c():
     global filename
     global expected
 
     filename = './support/case3_input.txt'
     expected = ["a1.c"]
-    
+
+
 @then('shows me "[a1.c]: Nenhum erro de análise estática foi encontrado"')
 def shows_nothing():
-
     global expected
 
     with open("output.txt",'r') as f_out:
         for line in f_out:
             assert "a1.c" in line
-            
+
             m = re.search('(\[[0-9A-Za-z\_]*\.\w\])\:\sNenhum erro de análise estática foi encontrado', line)
             assert m != None
