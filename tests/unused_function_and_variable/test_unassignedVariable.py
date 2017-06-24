e4cb6be8ce36ea4ad9aed22d52bb3d88922649f1
Gabriel Orisaka <orisaka@gmail.com>
2017-06-24 19:29:24 -0300

    Fix unassignedVariable case

--- tests/unused_function_and_variable/test_unassignedVariable.py
+++ tests/unused_function_and_variable/test_unassignedVariable.py
@@ -49,10 +49,8 @@ def receive_message():
     global filename
 
     with open("output.txt",'r') as f_out:
-
-        for line in f_out:
-            m = re.search('[\[\]\:\w\.\_]*\s(\(erro\) Variável não foi atribuída)', line)
-            assert m != None
+        m = re.search('[\[\]\:\w\.\_]*\s(\(erro\) Variável não foi atribuída)', f_out.read())
+        assert m != None
 
 @then('it doesn\'t show me "[<filename>.c:<linha>]: (erro) Variável não foi atribuída"')
 def shows_nothing( ):
