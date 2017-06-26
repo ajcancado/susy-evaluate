adcdf074048de730ca2cc7c63ccf82aafe12e3e1
Silvana Trindade <syletri@gmail.com>
2017-06-26 16:09:03 -0300

    Adding good and bad files

--- tests/support/format_string/wrongPrintfScanfParameterPositionError/bad.c
+++ tests/support/format_string/wrongPrintfScanfParameterPositionError/bad.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char const *argv[])
+void foo()
 {
+
+    int bar;
+
+    printf("%1$d\n");
+    printf("%1$d,%d,%4$d",1,2,3);
+    scanf("%2$d",&bar);
+    printf("%0$f", 0.0);
 }
--- tests/support/format_string/wrongPrintfScanfParameterPositionError/good.c
+++ tests/support/format_string/wrongPrintfScanfParameterPositionError/good.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void foo()
+{
+
+    int bar = 5;
+
+    printf("%d\n",bar);
+    scanf("%d",&bar);
+}
+
 int main(int argc, char const *argv[])
 {
+    foo();
+    return 0;
 }
