6fabb79bfb3639cb7a1bf3f006145b91b301922e
Silvana Trindade <syletri@gmail.com>
2017-06-27 17:56:26 -0300

    Fixing test

--- tests/support/input/case6/source.c
+++ tests/support/input/case6/source.c
@@ -3,6 +3,54 @@
 #include <string.h>
 #include "header.h"
 
+
+int f(unsigned int i)
+{
+    size_t s;
+    printf("%I64%i", s,i );
+
+    return 0;
+}
+
+int f2()
+{
+  printf("%f", 'a');
+}
+
+char *f3(char *p){
+    free(p);
+    return p;
+}
+
+char *f4()
+{
+    char *a = 0;
+    int i = 0;
+    for (int i = 0; i < 50; i++)
+    {
+        if (f1(i))
+        {
+            continue;   
+        }
+        a = realloc(a, i);
+        if (f2(i))
+        {
+            continue;
+        }
+    }
+
+    return a;
+}
+
+int f5(int argc, char const *argv[])
+{
+  FILE*f=fopen("myfile.txt",a);
+    free(f);
+
+    return 0;
+}
+
+
 int main(int argc, char const *argv[])
 {
   struct Books Book1;
